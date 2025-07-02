a3f8dcb1fee2ae98243b1c97ba6cc6bf646e58d3
Shifan Abdulla <shifan9914@gmail.com>
2025-07-02 23:33:09 +0530

    initial set up

---
+++ backend/feedback/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Feedback(models.Model):
+    name =models.CharField(max_length=50)
+    email = models.EmailField()
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_type = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.name} - {self.feedback}"
+    
+
